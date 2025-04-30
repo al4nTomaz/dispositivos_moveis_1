b3b4761b66a7ad1271b0996b436f7f9fa638f453
al4nTomaz <alantomazifprpgua@gmail.com>
2025-04-30 10:31:23 -0300

    feita aula de hoje parcial, tentando adequar ao padrão.

---
+++ lib/data/repositories/student_repository_contract.dart
@@ -0,0 +1,5 @@
+import 'package:dispositivos_moveis_1/common/errors/errors_classes.dart';
+import 'package:dispositivos_moveis_1/result.dart';
+import 'package:dispositivos_moveis_1/domain/studant_info_entity.dart';
+
+// abstract class StudentRepositoryContract {Future<Result<StudentInfoEntity,}
---
+++ lib/outros.dart
@@ -0,0 +1,8 @@
+
+import 'package:dispositivos_moveis_1/common/errors/errors_classes.dart';
+import 'package:dispositivos_moveis_1/domain/studant_info_entity.dart';
+
+@override 
+Future<Result<void, Failure> saveStudent(StudentInfoEntity student) async {
+  
+}
